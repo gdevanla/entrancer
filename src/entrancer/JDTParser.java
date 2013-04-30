8f57c942c641bc75ee342f661b4319c43d0579f5
Guru Devanla <grdvnl@gmail.com>
2013-04-30 06:57:09 -0500

    Clean up hard-coded values. Right now tool takes 3 arguments and runs like a charm

--- src/entrancer/JDTParser.java
+++ src/entrancer/JDTParser.java
@@ -3,19 +3,20 @@ package entrancer;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Properties;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.JavaModelException;
-import org.eclipse.jdt.core.Signature;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.ASTVisitor;
@@ -27,36 +28,49 @@ import org.eclipse.jdt.core.dom.SimpleName;
 import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 
 public class JDTParser {
-	JDKAPIDBAccess jdkApiDBAccess = new JDKAPIDBAccess();
-	//static String corpus = "/home/tathagata/Dropbox/research/entrancer/EAnciJDKSelfMethods.csv";
-	static String corpus = "/tmp/test.csv";
-	public static void parse(String str) throws JavaModelException {
+	
+	public final String baseFolder;
+	public final String fileContainingListOfClassToParse ;
+	public final String outputFile;
 
-		Properties prop = System.getProperties();
-		String[] classPathEntries = prop.getProperty("java.class.path").split(
-				":");
-		String[] sourcePathEntries = new String[] { "/home/tathagata/projects/entracer/code/" };
+	public void appendOutput(String s){
+		try {
+			PrintWriter out = new PrintWriter(new BufferedWriter(
+					new FileWriter(outputFile, true)));
+			out.print(s);
+			out.close();
+		} catch (IOException e) {
+
+		}
+		// oh noes!
+	}
+	
+	
+	public JDTParser(String baseFolder, String inputFile, String outputFile){
+		this.baseFolder = baseFolder;
+		this.fileContainingListOfClassToParse = inputFile;
+		this.outputFile = outputFile;
+	}
+
+
+	public void parse(String str) throws JavaModelException {
 
 		ASTParser parser = ASTParser.newParser(AST.JLS3);
 		parser.setResolveBindings(true);
 		parser.setKind(ASTParser.K_COMPILATION_UNIT);
 		parser.setSource(str.toCharArray());
-		parser.setEnvironment(classPathEntries, sourcePathEntries, null, false);
-		parser.setUnitName("codeanalyzer/src/entrancer/codeanalyzer/main/TestClass1.java");
-		
+
 
 		final CompilationUnit root = (CompilationUnit) parser.createAST(null);
-		final Set names = new HashSet(); 
+		final Set<String> names = new HashSet<String>(); 
 		
 		root.accept(new ASTVisitor() {
-			
-	
+
 			public boolean visit(ConstructorInvocation node) {
 				IMethodBinding methodBinding = node.resolveConstructorBinding();
-
+				String methodName = "";
 				if (methodBinding != null) {
 					StringBuilder str = new StringBuilder();
-					String methodName;
 					String[] methodNameSplit = node.resolveConstructorBinding()
 							.toString().split(" ");
 					if (methodNameSplit[1].equalsIgnoreCase("abstract")
@@ -89,62 +103,28 @@ public class JDTParser {
 							.replace("java.lang.", ""));
 
 					str.append(sbr + signature);
-					JDKAPIDBAccess jdkApiDBAccess = new JDKAPIDBAccess();
-					String doc = jdkApiDBAccess.getDocumentation(signature,
-							sbr.toString());
-					doc = doc.replaceAll("\n", "").replaceAll(".\n", "");
-					//System.out.println(doc);
+					appendOutput(str.toString());
 				}
 				return super.visit(node);
 			}
 
-			
 			public boolean visit(VariableDeclarationFragment node) {
 				SimpleName name = node.getName();
 				names.add(name.getIdentifier());
-				//System.out.println(name.propertyDescriptors(3).toString());
+				// System.out.println(name.propertyDescriptors(3).toString());
 				return false; // do not continue to avoid usage info
 			}
- 
+
 			public boolean visit(SimpleName node) {
 				if (names.contains(node.getIdentifier())) {
-				System.out.println(node + "\t");
+					System.out.println(node + "\t");
 				}
-				
-				try {
-					PrintWriter out = new PrintWriter(new BufferedWriter(
-							new FileWriter(corpus, true)));
-					out.print("  "+node+"  ");
-					out.close();
-				} catch (IOException e) {
-					// oh noes!
-				}
-				
+
+				appendOutput(" " + node + " ");
+
 				return true;
 			}
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
+
 			public boolean visit(MethodInvocation node) {
 				IMethodBinding methodBinding = node.resolveMethodBinding();
 
@@ -187,22 +167,7 @@ public class JDTParser {
 							.replace("java.lang.", ""));
 
 					str.append(sbr + signature);
-
-					JDKAPIDBAccess jdkApiDBAccess = new JDKAPIDBAccess();
-					String replace = jdkApiDBAccess.getDocumentation(signature,
-							sbr.toString()) + " ";
-					replace = replace.replaceAll("\n", "")
-							.replaceAll(".\n", "");
-					// System.out.print(replace);
-
-					try {
-						PrintWriter out = new PrintWriter(new BufferedWriter(
-								new FileWriter(corpus, true)));
-						out.print(replace);
-						out.close();
-					} catch (IOException e) {
-						// oh noes!
-					}
+					appendOutput(" " + str.toString() + " " );
 
 				}
 
@@ -230,63 +195,47 @@ public class JDTParser {
 		return fileData.toString();
 	}
 
-	public static void ParseFilesInDir() throws IOException, JavaModelException {
-		File dirs = new File(".");
-		String dirPath = dirs.getCanonicalPath() + File.separator + "src"
-				+ File.separator;
+	public String[] getFilesToParse(String fromFile) throws IOException{
 
-		// File root = new File(dirPath);
-		// File root = new
-		// File("/home/tathagata/projects/entracer/code/codeanalyzer/src/entrancer/codeanalyzer/main");
-		File root = new File(
-				"/home/tathagata/projects/entracer/coest/EAnci/cc/");
-		// System.out.println(root.listFiles());
-		File[] files = root.listFiles();
-		String filePath = null;
+		BufferedReader in
+			= new BufferedReader(new FileReader(fromFile));
 
-		for (File f : files) {
-			// System.out.println("Processing:"+f);
-			PrintWriter out;
-			try {
-				out = new PrintWriter(new BufferedWriter(new FileWriter(corpus,
-						true)));
-				out.print(f.toString().substring(49).replace(".txt", "") + "\t");
-				System.out.println();
-				out.close();
-			} catch (IOException e) {
-				// oh noes!
-			}
-
-			filePath = f.getAbsolutePath();
-			if (f.isFile()) {
-				parse(readFileToString(filePath));
-				try {
-					out = new PrintWriter(new BufferedWriter(new FileWriter(
-							corpus, true)));
-					out.println();
-					System.out.println();
-					out.close();
-				} catch (IOException ioe) {
-
-				}
-			}
-
+		List<String> files = new ArrayList<String>();
+		String currentLine = null;
+		while((currentLine = in.readLine()) != null){
+			files.add(currentLine);
 		}
-	}
 
-	public static void ParseSingleFile() throws IOException, JavaModelException {
-		// File f = new
-		// File("/home/tathagata/projects/entracer/code/codeanalyzer/src/entrancer/codeanalyzer/main/TestClass1.java");
-		File f = new File(
-				"/home/tathagata/projects/entracer/coest/EAnci/cc/EA149.txt");
-		parse(readFileToString(f.getAbsolutePath()));
+		return  files.toArray(new String[files.size()]);
 	}
 
 	public static void main(String args[]) throws IOException,
-			JavaModelException {
-		System.out.println("Start");
-		// ParseSingleFile();
-		ParseFilesInDir();
+	JavaModelException {
+		System.out.println("Start generating the corpus.");
 
+		if (args.length < 3){
+			System.out.println("Usage: java JDTParser <base-path-to-find-all-source-files> <file-containing-names-of-source-file <output-file>");
+			return;
+		}
+		
+		System.out.println("Called with " + args[0] + ", " + args[1] + ", " + args[2]);
+		JDTParser parser = new JDTParser(args[0], args[1], args[2]);
+		parser.parseAll();
+		System.out.println("Completed.");
+	}
+
+
+	private void parseAll() throws JavaModelException, IOException {
+		String[] sourceFiles = getFilesToParse(fileContainingListOfClassToParse);
+		
+		for (String s:sourceFiles){
+		    appendOutput("\n" + s + "\t");
+		    parse(readFileToString(getFilePath(s)));
+		}
+
+	}
+
+	private String getFilePath(String s) {
+		return new File(baseFolder, s).getAbsolutePath();
 	}
 }
